e937d92170fbca3f398e3be3b4e8392993021348
asifayyub <69436510+asifayyub@users.noreply.github.com>
2023-01-16 20:54:00 -0600

    Goldy Gopher - Description
    I have MVC framework. Please clone this repo and click on MVC_Application.sln. it will load the project in visual Studio

---
+++ MVC_Application/MVC_Application/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MVC_Application.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_Application.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Index(HomeModel home)
+        {
+            int lower = home.LowerBound;
+            int upper = home.UpperBound;
+
+            ViewBag.Lower = lower;
+            ViewBag.Upper = upper;
+
+            return View();
+
+        }
+
+       
+    }
+}
